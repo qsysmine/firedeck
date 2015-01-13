5f7e3045a931aee00be24fa858d25f05d94da0db
qsysmine <echomanari@gmail.com>
2015-01-13 07:14:52 -0800

    Fixed a possible future error.
    Did a conditional statement for the doColour function.

--- uiQuirks.js
+++ uiQuirks.js
@@ -1,5 +1,5 @@
 (function() {
-  var colour = "#";
+  var colour = "#FFF";
   if (location.hash == "#welcome") {
     console.log(location.hash);
     $('#main,#mainDash').prepend("<div class=\"alert alert-success alert-dismissible\" id=\"wAlert\" role=\"alert\"><button type=\"button\" class=\"close\" data-ui=\"Q\" data-uiQCl=\"#wAlert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>Welcome, <span class=\"usName\">[loading]</span></div>");
@@ -40,7 +40,7 @@
 
   };
   window.doColour = function() {
-    $('input.col').val(colour);
+    $('input.col').val((colour != null ? colour : "#FFF"));
   }
   var _modal = function(options) {
     if (options.close) {
